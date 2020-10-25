f89fe2db8bacc66760fcb07a7975c5e78b7440e6
niupengyu <505436991@qq.com>
2020-10-26 03:11:15 +0800

    优化

--- ahead-frame-core/src/main/java/com/github/niupengyu/core/util/InfoUtil.java
+++ ahead-frame-core/src/main/java/com/github/niupengyu/core/util/InfoUtil.java
@@ -1,7 +1,9 @@
 package com.github.niupengyu.core.util;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,8 +11,8 @@ public class InfoUtil {
 
     static String regex ="([1-9]\\d{5}(18|19|20)\\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\\d{3}[0-9Xx])|([1-9]\\d{5}\\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\\d{3})";
 
-    public static List<String> idCard(String text){
-        List<String> list = new ArrayList<>();
+    public static Set<String> idCard(String text){
+        Set<String> list = new HashSet<>();
         Pattern p = Pattern.compile(regex);
 
         Matcher m = p.matcher(text);
